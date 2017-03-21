62d6223f327a420612a29cbcfe806b635dde06d7
Tushar Tyagi <tushar4r@gmail.com>
2017-03-21 17:01:33 +0530

    Init

---
+++ todo/Program.fs
@@ -0,0 +1,50 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+module main
+
+open Todo
+
+[<EntryPoint>]
+let main argv =
+
+    let rec parseOptions args = 
+
+
+        (* The following options will be used:
+        c desc [state] [priority]-- create new todo
+        l           -- list all 
+        s <number>  -- change state of the task at number
+        p <number>  -- change priority of the given task
+        d <number>  -- change deadline of the given task
+        r <number>  -- reset the task, i.e. set to todo; remove priority and deadline
+        f <t|p|s|d> -- filter by date/priority/state
+        q <t|p|s>   -- (quick)sort by date/priority/state
+        h           -- show help
+        *)
+
+        match args with 
+        | [] -> ""
+        | "c"::desc::xs -> 
+            "Ask for description and run Todo.createNewTask" 
+        | "l"::xs -> 
+            "List all tasks" 
+        | "s"::num::state::xs -> 
+            "Change state task at number num" 
+        | "p"::num::priority::xs -> 
+            "Change priority" 
+        | "d"::deadline::priority::xs -> 
+            "Change deadline" 
+        | "f"::byWhat::criteria::xs -> 
+            "Filter" 
+        | "q"::byWhat::criteria::xs -> 
+            "Sort"
+        | "h"::xs -> 
+            "Show help"  
+        | x::xs -> 
+            "Option %s is unrecognised" 
+    
+    let argList = argv |> Seq.toList
+    printfn "%A" argList
+    printfn "%s" (parseOptions argList)
+    0 // return an integer exit code
---
+++ todo/Todo.fs
@@ -0,0 +1,75 @@
+﻿namespace Todo
+
+module Core = 
+
+    open System
+
+    type State =
+        | Done
+        | InProgress
+        | Waiting
+        | Todo
+
+
+    type Priority =
+        | High
+        | Medium
+        | Low
+
+
+    type Task = {
+        description: string
+        state: State
+        deadline: Option<DateTime>
+        priority: Option<Priority>
+        }
+
+    // Testing
+    let tasks = [
+        {description = "Wake Up"; state = Done; deadline = None; priority = Some High}
+        {description = "Bath"; state = Todo; deadline = None; priority = Some Low}
+        {description = "Breakfast"; state = Todo; deadline = Some DateTime.Today; priority = Some Medium}
+        ]
+
+    let createNewTask description = 
+        {description = description; state = Todo; deadline = None; priority = None}
+
+    let changeState newState task =
+        {task with state = newState}
+    
+    let changeDescription newDescription task =
+        {task with description = newDescription}
+
+    let changePriority newPriority task = 
+        { task with priority = Some newPriority }
+
+    let changeDeadline newDeadline task = 
+        { task with deadline = Some newDeadline }
+
+    let removePriority task = 
+        { task with priority = None }
+
+    let removeDeadline task = 
+        { task with deadline = None }
+
+    let resetTask = changeState Todo >> removePriority >> removeDeadline 
+
+    let printTask task = 
+        printf "%-10s | %-10A |" (task.description.PadRight(10)) task.state
+    
+        match task.deadline with
+        | Some date -> printf "%-15s |" (date.ToString("dd.MM.yy"))
+        | None -> printf "%-15s |" ""
+
+        match task.priority with
+        | Some priority -> printf "%10A" priority
+        | None -> printf "%10s" ""
+
+        printfn ""
+
+    let printAllTasks tasks = 
+        let printWithIndex i t = printfn
+        tasks |> Seq.iteri (fun i task->                         
+                                printf "%i. " (i + 1)
+                                printTask task )
+    
